61cbcaea560b25ec424993e0bc81db15fbaf4f24
Andrew Kosintsev <andrew.kosincev@gmail.com>
2017-04-06 11:37:54 +0200

    valid database name

--- files/apiman-db-create.sh
+++ files/apiman-db-create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "******CREATING APIMAN DATABASE******"
 # su -c "psql -d apiman -f /docker-entrypoint-initdb.d/apiman_postgresql.ddl"  -s /bin/sh postgres
-gosu  postgres postgres --single -j apiman < /docker-entrypoint-initdb.d/apiman_pg_new.ddl
+gosu  postgres postgres --single -j ApiManagerDS < /docker-entrypoint-initdb.d/apiman_pg_new.ddl
 echo ""
 echo "******APIMAN DATABASE CREATED******"
