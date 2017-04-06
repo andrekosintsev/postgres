3d8d9ca8d0d989488f2140d3b514663ab85458be
Andrew Kosintsev <andrew.kosincev@gmail.com>
2017-04-06 14:17:38 +0200

    init sql script

--- files/init.sql
+++ files/init.sql
@@ -1,3 +1,35 @@
-CREATE USER apiman;
-CREATE DATABASE ApiManagerDS;
-GRANT ALL PRIVILEGES ON DATABASE ApiManagerDS TO apiman;
+CREATE OR REPLACE FUNCTION inttobool(num int, val bool) RETURNS bool AS '
+    BEGIN
+        IF num=0 AND NOT val THEN
+            RETURN true;
+        ELSIF num<>0 AND val THEN
+            RETURN true;
+        ELSE
+            RETURN false;
+        END IF;
+    END;
+' LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION inttobool(val bool, num int) RETURNS bool AS '
+    BEGIN
+        RETURN inttobool(num,val);
+    END;
+' LANGUAGE 'plpgsql';
+
+
+DROP OPERATOR IF EXISTS = (integer, boolean);
+CREATE OPERATOR = (
+     leftarg = integer,
+     rightarg = boolean,
+     procedure = inttobool,
+     commutator = =,
+     negator = !=
+);
+DROP OPERATOR IF EXISTS = (boolean, integer);
+CREATE OPERATOR = (
+     leftarg = boolean,
+     rightarg = integer,
+     procedure = inttobool,
+     commutator = =,
+     negator = !=
+);
